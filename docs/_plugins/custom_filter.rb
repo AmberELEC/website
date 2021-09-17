8699ac71eefc149cfd0f2fff58473378bfef4682
YonKuma <kumarei@gmail.com>
2021-09-17 09:59:51 -0400

    Dev testing

---
+++ docs/_plugins/custom_filter.rb
@@ -0,0 +1,30 @@
+module Jekyll::CustomFilter
+
+	def root_category_for_page(page, categories)
+		categories.each do | label, category |
+			if page_in_category(page, category)
+				return category
+			end
+		end
+	end
+
+	def page_in_category(page, category)
+		if category.pages
+			for page in category.pages
+				if page.url == page
+					return true
+				end
+			end
+		end
+		if category.categories
+			for subcategory in category.categories
+				if page_in_category(page, subcategory)
+					return true
+				end
+			end
+		end
+		return false
+	end
+end
+
+Liquid::Template.register_filter(Jekyll::CustomFilter)
